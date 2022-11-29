4a2889e03be7072fc50e006e83ed0da87a187a2e
Aristotelis Manios <A.Manios@acg.edu>
2022-11-29 19:43:11 +0200

    Code format changes on ticket file

--- tickets.py
+++ tickets.py
@@ -1,13 +1,14 @@
 import random
 
-class ticket:
 
-    def __init__(self,labels,description):
-        self.ticketID = random.randint(1111,9999)
+class Ticket:
+
+    def __init__(self, labels, description):
+        self.ticketID = random.randint(1111, 9999)
         self.labels = labels
         self.description = description
 
-    #Setters
+    # Setters
     def setTicketID(self, ticketIDinput):
         self.ticketID = ticketIDinput
 
@@ -17,31 +18,33 @@ class ticket:
     def setDescription(self, descriptionInput):
         self.description = descriptionInput
 
-    #Getters
+    # Getters
 
     def getTicketID(self):
-        print (self.ticketID)
+        print(self.ticketID)
 
     def getLabels(self):
-       print (self.labels)
+        print(self.labels)
 
     def getDescription(self):
-        print (self.description)
-class ticketFunctions:
+        print(self.description)
 
-    def createTicket():
-        #user here is asked for the name of the file/ticket
+
+class TicketFunctions:
+
+    def createTicket(self):
+        # user here is asked for the name of the file/ticket
         ticketTitle = input("Please enter the ticket title: ")
 
-        userLabels = input("Please enter the labels for the new ticket: ")
+        userlabels = input("Please enter the labels for the new ticket: ")
         ticketDescription = input("Describe the problem: ")
-        #Ticket object that will be put in a file
-        newTicket = ticket(userLabels, ticketDescription)
+        # Ticket object that will be put in a file
+        newTicket = Ticket(userlabels, ticketDescription)
 
         f = open(f"{ticketTitle}.txt", 'w')
         f.write(newTicket.labels + "\n")
         f.close()
 
-        f = open (f"{ticketTitle}.txt" , 'a')
-        f.write (newTicket.description)
+        f = open(f"{ticketTitle}.txt", 'a')
+        f.write(newTicket.description)
         f.close()
