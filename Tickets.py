4c208521771d522dd8523a62a23d5b0a585dabb5
Aristotelis Manios <A.Manios@acg.edu>
2022-12-04 22:40:19 +0200

    Created menus for different types of users

--- Tickets.py
+++ Tickets.py
@@ -197,3 +197,129 @@ def delete_ticket(ticket):
     except (FileNotFoundError, ValueError) as error:
         print(error)
         print("The ticket could not be found, check the spelling and try again")
+
+
+def print_ticket_menu_manager():
+    print(
+        f"\n============================\n"
+        f"1: Create a new ticket\n"
+        f"2: See all tickets\n"
+        f"3: Display ticket\n"
+        f"4: Show all tickets of one employee\n"
+        f"5: Modify ticket\n"
+        f"6: Delete ticket\n"
+        f"0: close the menu"
+        f"\n============================\n")
+
+
+def ticket_menu_for_manager():
+    print_ticket_menu_manager()
+    user_input = int(input("Please enter your choice: "))
+    while user_input != 0:
+        try:
+            if user_input == 1:
+                create_ticket()
+
+            elif user_input == 2:
+                show_all_tickets()
+
+            elif user_input == 3:
+                ticket_to_display = input("Please type the name of the ticket you wish to see: ")
+                display_specific_ticket(ticket_to_display)
+
+            elif user_input == 4:
+                employee_num = input("Please enter 1 for employee 1 or 2 for employee 2: ")
+                show_tickets_by_employee(employee_num)
+
+            elif user_input == 5:
+                ticket_name = input("Please enter the name of the ticket to be modified: ")
+                modify_ticket(ticket_name)
+
+            elif user_input == 6:
+                ticket_name = input("Please enter the name of the ticket to be deleted: ")
+                delete_ticket(ticket_name)
+
+            else:
+                print("Non valid option")
+
+            print_ticket_menu_manager()
+            user_input = int(input("Please enter your choice: "))
+        except ValueError as error:
+            print(error)
+            print("An error has occurred, please try again")
+
+
+def print_ticket_menu_employee():
+    print(
+        f"\n============================\n"
+        f"1: Create a new ticket\n"
+        f"2: See all tickets\n"
+        f"3: Display ticket\n"
+        f"4: Delete ticket\n"
+        f"0: close the menu"
+        f"\n============================\n")
+
+
+def ticket_menu_for_employee():
+    print_ticket_menu_employee()
+    user_input = int(input("Please enter your choice: "))
+    while user_input != 0:
+        try:
+            if user_input == 1:
+                create_ticket()
+
+            elif user_input == 2:
+                show_all_tickets()
+
+            elif user_input == 3:
+                ticket_to_display = input("Please type the name of the ticket you wish to see: ")
+                display_specific_ticket(ticket_to_display)
+
+            elif user_input == 4:
+                ticket_name = input("Please enter the name of the ticket to be deleted: ")
+                delete_ticket(ticket_name)
+
+            else:
+                print("Non valid option")
+
+            print_ticket_menu_employee()
+            user_input = int(input("Please enter your choice: "))
+        except ValueError as error:
+            print(error)
+            print("An error has occurred, please try again")
+
+
+def print_ticket_menu_user():
+    print(
+        f"\n============================\n"
+        f"1: Create a new ticket\n"
+        f"2: See all tickets\n"
+        f"3: Display ticket\n"
+        f"0: close the menu"
+        f"\n============================\n")
+    pass
+
+
+def ticket_menu_for_user():
+    print_ticket_menu_user()
+    user_input = int(input("Please enter your choice: "))
+    while user_input != 0:
+        try:
+            if user_input == 1:
+                create_ticket()
+
+            elif user_input == 2:
+                show_all_tickets()
+
+            elif user_input == 3:
+                ticket_to_display = input("Please type the name of the ticket you wish to see: ")
+                display_specific_ticket(ticket_to_display)
+
+            else:
+                print("Non valid option")
+
+            print_ticket_menu_user()
+            user_input = int(input("Please enter your choice: "))
+        except ValueError as error:
+            print(error)
+            print("An error has occurred, please try again")
