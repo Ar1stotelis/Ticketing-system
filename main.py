21fa8e5d096d500beea722f8432bd0087ba31e43
GeorgeStudent <97541988+GeorgeStudent@users.noreply.github.com>
2022-12-04 23:25:29 +0200

    Opening menu to verify who is using the software

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from Tickets import *
 
 if __name__ == "__main__":
-    ticket_menu_for_manager()
-    ticket_menu_for_employee()
-    ticket_menu_for_user()
+    print("1. Manager")
+    print("2. Employee")
+    print("3. User")
+    entity_input = int(0)
+    try:
+        entity_input = int(input("Who is using the software : "))
+        while 1 > entity_input > 3:
+            print("Error")
+            print("Choose one of the following")
+            entity_input = int(input("Who is using the software : "))
+
+        if entity_input == 1:
+            ticket_menu_for_manager()
+        elif entity_input == 2:
+            ticket_menu_for_employee()
+        else:
+            ticket_menu_for_user()
+
+    except ValueError as error:
+        print("Error")
+        print("Invalid input")
+
