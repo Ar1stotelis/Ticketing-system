f1551218cfb09ac7abd23e92c5804bcd331e0483
GeorgeStudent <97541988+GeorgeStudent@users.noreply.github.com>
2022-12-04 19:08:41 +0200

    Corrected the naming conversion (Snake)

--- EmployeeInterfaceClass.py
+++ EmployeeInterfaceClass.py
@@ -1,41 +1,40 @@
 from EmployeeClass import Employee
 
+# class EmployInterface
+class EmployInterface:
+    employ = Employ("EmployName", "EmployIdentifier", "EmployPhoneNumber", "EmployEmail", 0)
 
-#class EmployeeInterface
-class EmployeeInterface:
-    employ = Employee("EmployName", "EmployIdentifier", "EmployPhoneNumber", "EmployEmail", 0)
-    
-    def SetName(self, nameInput):
-        self.employ.SetName(nameInput)
-        
-    def SetIdentifier(self, identifierInput):
-        self.employ.SetIdentifier(identifierInput)
+    def set_name(self, name_input):
+        self.employ.set_name(name_input)
 
-    def SetPhoneNumber(self, phoneNumberInput):
-        self.employ.SetPhoneNumber((phoneNumberInput))
+    def set_identifier(self, identifier_input):
+        self.employ.set_identifier(identifier_input)
 
-    def SetEmail(self, emailInput):
-        self.employ.SetEmail(emailInput)
+    def set_phone_number(self, phone_number_input):
+        self.employ.set_phone_number((phone_number_input))
 
-    def SetNumberOfTickets(self, numberOfTicketsInput):
-        self.employ.SetNumberOfTickets(numberOfTicketsInput)
+    def set_email(self, email_input):
+        self.employ.set_email(email_input)
 
-    #Getters -----
+    def set_number_of_tickets(self, number_of_tickets_input):
+        self.employ.set_number_of_tickets(number_of_tickets_input)
 
-    def GetName(self):
-        return self.employ.Getname()
+    # Getters -----
 
-    def GetIdentifier(self):
-        return self.employ.GetIdentifier()
+    def get_name(self):
+        return self.employ.get_name()
 
-    def GetPhoneNumber(self):
-        return self.employ.GetPhoneNumber()
+    def get_identifier(self):
+        return self.employ.get_identifier()
 
-    def GetEmail(self):
-        return self.employ.GetEmail()
+    def get_phone_number(self):
+        return self.employ.get_phone_number()
 
-    def GetNumberOfTickets(self):
-        return self.employ.GetNumberOfTickets()
+    def get_email(self):
+        return self.employ.get_email()
 
-    def To_String(self):
-        return self.employ.To_String()
+    def get_number_of_tickets(self):
+        return self.employ.get_number_of_tickets()
+
+    def to_string(self):
+        return self.employ.to_string()
