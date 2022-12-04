7f14f66082c02926e382ae0f41e65222e1c581a2
Mili002 <97444422+Mili002@users.noreply.github.com>
2022-12-04 20:27:10 +0200

    Update labels.py
    Creating the class labels in order for every user to choose his label or delete a label etc

--- labels.py
+++ labels.py
@@ -1,3 +1,36 @@
-from enum import Enum
+class Labels:
+    ticket_labels = []
 
-class Labels(Enum):
+    def __init__(self, labels_array):
+        self.ticket_labels = labels_array.copy()
+
+    def add_label(self):
+        label_input = input("Please, enter the name of the new label :")
+        self.ticket_labels.append(label_input)
+
+    def delete_label(self):
+        i = int(1)
+        j = int(0)
+        for n in self.ticket_labels:
+            print(i, ". ", self.ticket_labels[j])
+            i+=1
+            j+=1
+
+
+
+        i = int(input("Please, choose the label you wish to delete : "))
+        j = 0
+        flag = False
+        i-= 1
+        while(i > -1 and i < len(self.ticket_labels)):
+            if i == j:
+                del self.ticket_labels[j]
+                i = -1
+                flag = True
+            j+= 1
+
+        if flag:
+            print("Label deleted")
+
+    def print_labels(self):
+        print(self.ticket_labels)
