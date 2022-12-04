55fd748f777f790d3ee60b94f43f86f50ace5cd4
GeorgeStudent <97541988+GeorgeStudent@users.noreply.github.com>
2022-12-04 19:07:11 +0200

    Corrected variable and method names up to standards
    I have used the Camel Case and in the python docs it says the convention is the Snake

--- EmployeeClass.py
+++ EmployeeClass.py
@@ -1,54 +1,93 @@
-#Needs a ticket member variable and the appropriate methods
+# Needs a ticket member variable and the appropriate methods
 
-#Employ class
-class Employee:
+# Employ class
+class Employ:
     name = "EmployName"
     identifier = "EmployIdentifier"
-    phoneNumber = "EmployPhoneNumber"
+    phone_number = "EmployPhoneNumber"
     email = "EmployEmail"
-    numberOfTickets = 0
+    number_of_tickets = 0
 
-    def __init__(self, name, identifier, phoneNumber, email, numberOfTickets):
+    def __init__(self, name, identifier, phone_number, email, number_of_tickets):
         self.name = name
         self.identifier = identifier
-        self.phoneNumber = phoneNumber
+        self.phone_number = phone_number
         self.email = email
-        self.numberOfTickets = numberOfTickets
+        self.number_of_tickets = number_of_tickets
 
-    #Setters -----
+    # Setters -----
 
-    def SetName(self, nameInput):
-        self.name = nameInput
+    def set_name(self, name_input):
+        self.name = name_input
 
-    def SetIdentifier(self, identifierInput):
-        self.identifier = identifierInput
+    def set_identifier(self, identifier_input):
+        self.identifier = identifier_input
 
-    def SetPhoneNumber(self, phoneNumberInput):
-        self.phoneNumber = phoneNumberInput
+    def set_phone_number(self, phone_number_input):
+        self.phone_number = phone_number_input
 
-    def SetEmail(self, emailInput):
-        self.email = emailInput
+    def set_email(self, email_input):
+        self.email = email_input
 
-    def SetNumberOfTickets(self, numberOfTicketsInput):
-        self.numberOfTickets = numberOfTicketsInput
+    def set_number_of_tickets(self, number_of_tickets_input):
+        self.number_of_tickets = number_of_tickets_input
 
-    #Getters -----
+    # Getters -----
 
-    def GetName(self):
+    def get_name(self):
         return self.name
 
-    def GetIdentifier(self):
+    def get_identifier(self):
         return self.identifier
 
-    def GetPhoneNumber(self):
-        return self.phoneNumber
+    def get_phone_number(self):
+        return self.phone_number
 
-    def GetEmail(self):
+    def get_email(self):
         return self.email
 
-    def GetNumberOfTickets(self):
-        return self.numberOfTickets
+    def get_number_of_tickets(self):
+        return self.number_of_tickets
 
-    def To_String(self):
+    def to_string(self):
         return f"Name : {self.name}\nIdentifier : {self.identifier}\nPhone number : {self.phoneNumber}\nEmail : {self.email}\nNumber of Tickets : {self.numberOfTickets}\n"
 
+
+# class EmployInterface
+class EmployInterface:
+    employ = Employ("EmployName", "EmployIdentifier", "EmployPhoneNumber", "EmployEmail", 0)
+
+    def set_name(self, name_input):
+        self.employ.set_name(name_input)
+
+    def set_identifier(self, identifier_input):
+        self.employ.set_identifier(identifier_input)
+
+    def set_phone_number(self, phone_number_input):
+        self.employ.set_phone_number((phone_number_input))
+
+    def set_email(self, email_input):
+        self.employ.set_email(email_input)
+
+    def set_number_of_tickets(self, number_of_tickets_input):
+        self.employ.set_number_of_tickets(number_of_tickets_input)
+
+    # Getters -----
+
+    def get_name(self):
+        return self.employ.get_name()
+
+    def get_identifier(self):
+        return self.employ.get_identifier()
+
+    def get_phone_number(self):
+        return self.employ.get_phone_number()
+
+    def get_email(self):
+        return self.employ.get_email()
+
+    def get_number_of_tickets(self):
+        return self.employ.get_number_of_tickets()
+
+    def to_string(self):
+        return self.employ.to_string()
