40b56ead48e297ab49fb7585c6e5f193d39b05a3
Mili002 <97444422+Mili002@users.noreply.github.com>
2022-12-04 19:01:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,57 @@
-# MENU main programm
+import Tickets.py
 
-While
-True:
 
-print(' =================================')
-print('       CHOOSE AN OPTION \n')
-print('(type the first number of your choice')
-print(' =====================================')
+def print_ticket_menu():
 
-try:
-    choice = input(' 1. CREATE A TICKET \n 2. STATUS OF THE TICKET \n ')
-    choice = int(choice)
-except:
-    print(' \n \n error, give me a numeric input please. \n')
+    
+    print(
+        f"\n============================\n"
+        f"1: Create a new ticket\n"
+        f"2: See all tickets\n"
+        f"3: Display ticket\n"
+        f"4: Show all tickets of one employee\n"
+        f"5: Modify ticket\n"
+        f"6: Delete ticket\n"
+        f"0: close the menu"
+        f"\n============================\n")
 
-break
 
-While
-True:
+def ticket_menu():
+    print_ticket_menu()
+    user_input = int(input("Please enter your choice: "))
+    while user_input != 0:
+        try:
+            if user_input == 1:
+                create_ticket()
 
-match choice:
-    case "1":
-    # call the selected function
+            elif user_input == 2:
+                show_all_tickets()
 
-    case "2":
-        # embedded condition in order to identify if the user has opened a ticket before
-        while True:
-            print(' ===================================')
-            print('       STATUS OF YOUR TICKET \n')
-            print('(type the first number of your choice')
-            print('=====================================')
+            elif user_input == 3:
+                ticket_to_display = input("Please type the name of the ticket you wish to see: ")
+                display_specific_ticket(ticket_to_display)
 
-            try:
-                choice = input(' 1. MODIFY TICKET \n 2. DELETE TICKET \n ')
-                choice = int(choice)
-            except:
-                print(' \n \n error, give me a numeric input please. \n')
+            elif user_input == 4:
+                employee_num = input("Please enter 1 for employee 1 or 2 for employee 2: ")
+                show_tickets_by_employee(employee_num)
 
-            break
+            elif user_input == 5:
+                ticket_name = input("Please enter the name of the ticket to be modified: ")
+                modify_ticket(ticket_name)
 
-        # menu to choose if the user wants to modify the ticket or delete it
+            elif user_input == 6:
+                ticket_name = input("Please enter the name of the ticket to be deleted: ")
+                delete_ticket(ticket_name)
+
+            else:
+                print("Non valid option")
+
+            print_ticket_menu()
+            user_input = int(input("Please enter your choice: "))
+        except ValueError as error:
+            print(error)
+            print("An error has occurred, please try again")
+
+
+if __name__ == "__main__":
+    ticket_menu()
